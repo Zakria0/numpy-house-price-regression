4640ed2da05bb06952f9fa0fb7895530aa7d4258
Oulhadj Zakaria <69174113+Zakria0@users.noreply.github.com>
2026-09-13 16:01:20 +0100

    Step 13: ols_fit

--- model.py
+++ model.py
@@ -85,8 +85,13 @@ def partition_indices(indices, train_ratio, val_ratio):
 def subset_xy(X, y, indices):
     return X[indices], y[indices]
 
-# Step 13 - ols_fit (not yet solved)
-# TODO: implement
+# Step 13 - ols_fit
+def ols_fit(X, y):
+    A = X.T @ X
+    b = X.T @ y
+    theta = np.linalg.solve(A, b)
+
+    return theta
 
 # Step 14 - ols_predict (not yet solved)
 # TODO: implement
