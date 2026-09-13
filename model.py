0930ef570d792f2a78d6e2829c939da9c825bedd
Oulhadj Zakaria <69174113+Zakria0@users.noreply.github.com>
2026-09-13 12:02:10 +0100

    Step 3: clip_columns

--- model.py
+++ model.py
@@ -32,8 +32,9 @@ def compute_iqr_bounds(X, k=1.5):
 
     return lower, upper
 
-# Step 3 - clip_columns (not yet solved)
-# TODO: implement
+# Step 3 - clip_columns
+def clip_columns(X, lower, upper):
+    return np.clip(X, lower, upper)
 
 # Step 4 - make_ratio_feature (not yet solved)
 # TODO: implement
